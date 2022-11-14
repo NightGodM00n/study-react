31bb9cd34ad31b1eb67bffab412cb463da06f33a
NightGodM00n <noriaki.nakaji61@gmail.com>
2022-11-14 20:51:48 +0900

    ♻️ Mainをコンポーネント化

--- components/Headline.jsx
+++ components/Headline.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css';
 
-export default function Headline(props) {
+export function Headline(props) {
+    console.log(props);
     return (
         <div>
             <h1 className={styles.title}>
@@ -8,9 +9,9 @@ export default function Headline(props) {
             </h1>
 
             <p className={styles.description}>
-                Get started by editing{' '}
-                <code className={styles.code}>pages/{props.page}.js</code>
+                Get started by editing {props.children}
             </p>
+            <button onClick={props.onClick}>ボタン</button>
         </div>
     )
 }
